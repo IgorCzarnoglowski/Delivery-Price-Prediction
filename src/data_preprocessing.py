122907cccbc7a3944087c54768fb662c1d572f51
DjCr4y0n <igor.czarnoglowski.1ap@gmail.com>
2026-04-07 13:42:48 +0200

    Add encoding method
    add two ways of encoding one-hot encoding and label encoding

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder
 
 def remove_unused_columns(df: pd.DataFrame):
     print('Removing columns...')
@@ -8,6 +9,27 @@ def remove_unused_columns(df: pd.DataFrame):
         if c in df.columns:
             df.drop(c, axis=1, inplace=True)
             print(f'Removed: {c}')
+
+def encode_categorical_values(df: pd.DataFrame):
+    print('Encoding categorical features...')
+
+    categorical_columns= df.select_dtypes(include='object').columns
+
+    for col in categorical_columns:
+        # One hot encoding for less than 6 features
+        if df[col].nunique() <= 6:
+            dummies = pd.get_dummies(df[col], prefix=col, drop_first=True)
+            df = pd.concat([df, dummies], axis=1)
+            df.drop(col, axis=1, inplace=True)
+        # Label encoding for more than 6
+        else:
+            le = LabelEncoder()
+            df[col] = le.fit_transform(df[col])
+
+    return df
+
+
+
 
 
 
