2bde9b3d09edfeba8f968de6dc12f72092c48cff
DjCr4y0n <igor.czarnoglowski.1ap@gmail.com>
2026-04-07 16:54:03 +0200

    rename distance column

--- src/data_loader.py
+++ src/data_loader.py
@@ -6,4 +6,6 @@ DATASET_DIR = os.path.join(os.getcwd(), '..', 'data')
 def load_data():
     df = pd.read_csv(os.path.join(DATASET_DIR, 'Delivery_Logistics.csv'))
 
+    df.rename(columns={'distance_km': 'delivery_distance_km'})
+
     return df
