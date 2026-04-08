7b64895f5c34489539fce2417323dfa5cf6d4427
DjCr4y0n <igor.czarnoglowski.1ap@gmail.com>
2026-04-08 17:38:17 +0200

    little fix

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-DATASET_DIR = os.path.join(os.getcwd(), '..', 'data')
+DATASET_DIR = os.path.join(os.getcwd(), 'data')
 
 def load_data():
     df = pd.read_csv(os.path.join(DATASET_DIR, 'Delivery_Logistics.csv'))
